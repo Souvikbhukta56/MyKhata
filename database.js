56937ad2669a0dd0779e1b250ac79da3aa5dc7ad
Souvikbhukta56 <87611200+Souvikbhukta56@users.noreply.github.com>
2023-12-18 22:19:16 +0530

    Update database.js

--- database.js
+++ database.js
@@ -2,10 +2,10 @@ import { initializeApp } from 'firebase/app';
 import { getDatabase, ref, set} from 'firebase/database';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAe2LSZvMioVjcgcwDKMczd3im8HcXIvVQ",
-    databaseURL: "https://my-khata-28996-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: 'my-khata-28996',
-    appId: "1:32043139222:android:4f51a73bdca3b7c5c3c2b7" 
+    apiKey: "Your Firebase api key",
+    databaseURL: "Your Firebase database url",
+    projectId: 'Your Firebase project id',
+    appId: "Your Firebase app id" 
 };
   
 initializeApp(firebaseConfig);
